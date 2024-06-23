4d0387eaaf72c26e2e70b2919457f7f15a0e6873
ketansharma22 <kanu220504@gmail.com>
2024-06-23 23:05:49 +0530

    token verification half done

--- backend/utils/token-manager.js
+++ backend/utils/token-manager.js
@@ -1,15 +1,19 @@
 import jwt from 'jsonwebtoken'
-import { config } from 'dotenv'
 import { COOKIE_NAME } from './constants'
+import { config } from 'dotenv'
 config()
+
 export const createToken= (id,email,expiresIn)=>{
     const token=jwt.sign({id:id,email:email},process.env.JWT_SECRET ,{
         expiresIn:expiresIn,
     })
     return token;
 }
-
 export const verifyToken=async(req,res,next)=>{
     const token=req.signedCookies[`${COOKIE_NAME}`]
-    console.log(token);
+    if (!token || token.trim() === "") {
+        return res.status(401).json({ message: "Token Not Received" });
+      }
+      console.log(token);
 }
+
