dc76e9f96caedc31d6dbaaa431a6ada4b294ce98
ketansharma22 <kanu220504@gmail.com>
2024-07-11 18:47:10 +0530

    some

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,9 @@ app.use(cors({origin:"http://localhost:5173", credentials:true}))
 app.use(express.json())
 app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use(morgan("dev"))
-
+app.get("/",(req,res)=>{
+    res.send("welcome")
+})
 app.use('/api/v1',appRouter)
 
 export default app;
