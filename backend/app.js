9f2bf638e9d358d848c32d3b65a41e6a576826dc
ketansharma22 <kanu220504@gmail.com>
2024-07-11 21:53:18 +0530

    m

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ config()
 
 const app= express();
 
-app.use(cors({origin:"http://localhost:5173", credentials:true}))
+app.use(cors({origin:"https://cheat-chatfrontend-ketansharma22s-projects.vercel.app", credentials:true}))
 app.use(express.json())
 app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use(morgan("dev"))
