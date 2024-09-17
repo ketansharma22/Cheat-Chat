641298b33c8ccb6f5a5bec380209986d79e7c156
ketansharma22 <kanu220504@gmail.com>
2024-09-17 12:33:56 +0530

    main

--- frontend/Botapp/src/main.jsx
+++ frontend/Botapp/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./Context/AuthContext.jsx";
 import axios from "axios";
 import { Toaster } from "react-hot-toast";
-axios.defaults.baseURL="http://localhost:5000/api/v1"
+axios.defaults.baseURL="http://localhost:5000/api/v1"||"https://cheat-chatf-ketansharma22s-projects.vercel.app/"
 axios.defaults.withCredentials= true;
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
