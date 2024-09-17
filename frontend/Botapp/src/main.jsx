abb9be71e310aa904229b0c3cec68af76a5ae7cc
ketansharma22 <kanu220504@gmail.com>
2024-09-17 17:14:47 +0530

    main

--- frontend/Botapp/src/main.jsx
+++ frontend/Botapp/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./Context/AuthContext.jsx";
 import axios from "axios";
 import { Toaster } from "react-hot-toast";
-axios.defaults.baseURL="https://cheat-chat-production.up.railway.app/"
+axios.defaults.baseURL="https://cheat-chat-production.up.railway.app/api/v1"
 axios.defaults.withCredentials= true;
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
